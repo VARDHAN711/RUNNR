28170f015b6bac4c200edea8ad57fe2751d018bb
Vardhan711 <mouryavardhanbk711@gmail.com>
2026-04-15 12:22:24 +0530

    update

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,9 +1,18 @@
+// frontend/eslint.config.js
 import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+export default [
+  js.configs.recommended,
   pluginReact.configs.flat.recommended,
-]);
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+];
